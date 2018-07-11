6dfa42c8909b64d4e3bd85630ed14de3bc8a98ca
pheel-up <plw1980@gmail.com>
2018-07-11 15:58:17 -0400

    added functionality for retrieving data from twitter and spotify

---
+++ liri.js
@@ -0,0 +1,103 @@
+// DEPENDENCIES
+// =====================================
+// Read and set environment variables
+require("dotenv").config();
+
+// Import the Twitter NPM package.
+var Twitter = require("twitter");
+
+// Import the node-spotify-api NPM package.
+var Spotify = require("node-spotify-api");
+
+// Import the API keys
+var keys = require("./keys");
+
+// Import the request npm package.
+var request = require("request");
+
+// Import the FS package for read/write.
+var fs = require("fs");
+
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+
+console.log(spotify);
+console.log(client);
+
+// Function for running a Spotify search
+var getMeSpotify = function(songName) {
+  if (songName === undefined) {
+    songName = "Futureworld";
+  }
+
+  spotify.search(
+    {
+      type: "track",
+      query: songName
+    },
+    function(err, data) {
+      if (err) {
+        console.log("Error occurred: " + err);
+        return;
+      }
+
+      var songs = data.tracks.items;
+
+      for (var i = 0; i < songs.length; i++) {
+        console.log(i);
+        console.log("artist(s): " + songs[i].artists.map(getArtistNames));
+        console.log("song name: " + songs[i].name);
+        console.log("preview song: " + songs[i].preview_url);
+        console.log("album: " + songs[i].album.name);
+        console.log("-----------------------------------");
+      }
+    }
+  );
+};
+
+// Function for running a Twitter Search
+var getMyTweets = function(screenName) {
+  var client = new Twitter(keys.twitter);
+
+  var params = {
+    screen_name: screenName
+  };
+  client.get("statuses/user_timeline", params, function(error, tweets, response) {
+    if (!error) {
+      for (var i = 0; i < tweets.length; i++) {
+        console.log(tweets[i].created_at);
+        console.log("");
+        console.log(tweets[i].text);
+      }
+    }
+  });
+};
+
+// Function for determining which command is executed
+var choose = function(caseData, functionData) {
+  switch (caseData) {
+  case "my-tweets":
+    getMyTweets(functionData);
+    break;
+  case "spotify-this-song":
+    getMeSpotify(functionData);
+    break;
+  case "movie-this":
+    getMeMovie(functionData);
+    break;
+  case "do-what-it-says":
+    doWhatItSays();
+    break;
+  default:
+    console.log("LIRI doesn't know that");
+  }
+};
+
+// Function which takes in command line arguments and executes correct function accordingly
+var runThis = function(argOne, argTwo) {
+  choose(argOne, argTwo);
+};
+
+// MAIN PROCESS
+// =====================================
+runThis(process.argv[2], process.argv[3]);
